f656b83c9332fa59099defb363be24a6bfab933a
edwardmf94 <edward@resit.cl>
2023-03-29 23:26:04 -0500

    [FIX] l10n_pe_edi_pse_factura: Se agrega funcion para enviar comunicacion de baja cuando se esta consultado estado de factura

--- l10n_pe_edi_pse_factura/models/account_edi_format.py
+++ l10n_pe_edi_pse_factura/models/account_edi_format.py
@@ -218,8 +218,8 @@ class AccountEdiFormat(models.Model):
                     if line.discount>0 and line.discount<100:
                         _item['descuento_tipo']=line.l10n_pe_edi_allowance_charge_reason_code if line.l10n_pe_edi_allowance_charge_reason_code else '00'
                         _item['descuento_factor']=(line.discount or 0.0) / 100.0
-                        _item['descuento_base']=line.price_subtotal/(1.0 - _item['discount_factor'])
-                        _item['descuento_importe']=_item['discount_base'] * _item['discount_factor']
+                        _item['descuento_base']=line.price_subtotal/(1.0 - _item['descuento_factor'])
+                        _item['descuento_importe']=_item['descuento_base'] * _item['descuento_factor']
 
                     if isc_amount>0:
                         _item['tipo_de_calculo_isc'] = isc_type
