0be6dac152c6448869afe4918a64f9f6841ae308
edwardmf94 <edward@resit.cl>
2023-04-12 22:20:08 -0500

    [FIX] se soluciona error al descargar cdr

--- l10n_pe_edi_pse_factura/models/account_move.py
+++ l10n_pe_edi_pse_factura/models/account_move.py
@@ -90,8 +90,10 @@ class AccountMove(models.Model):
             })
 
     def _retry_edi_documents_error_hook(self):
-        for move in self.filtered(lambda m: m.l10n_pe_edi_pse_uid and m.l10n_pe_edi_pse_status=='ask_for_status'):
+        for move in self.filtered(lambda m: m.l10n_pe_edi_pse_uid and (m.l10n_pe_edi_pse_status=='ask_for_status')):
             move.edi_document_ids.filtered(lambda d: d.state in ('sent')).write({'state': 'to_send'})
+        for move in self.filtered(lambda m: m.l10n_pe_edi_pse_cancel_uid and (m.l10n_pe_edi_pse_void_status=='ask_for_status')):
+            move.edi_document_ids.filtered(lambda d: d.state in ('cancelled')).write({'state': 'to_cancel'})
 
     def button_cancel(self):
         pe_edi_format = self.env.ref('l10n_pe_edi_pse_factura.edi_pe_pse')
