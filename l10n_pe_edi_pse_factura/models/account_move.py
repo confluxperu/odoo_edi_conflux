0ebfc35a6bd5131bfaef9d4158522d3e02e82baa
Edwar Ruben <edwardmf94@gmail.com>
2026-04-16 00:59:45 -0500

    [FIX] se ajusta montos en calculo detracciones

--- l10n_pe_edi_pse_factura/models/account_move.py
+++ l10n_pe_edi_pse_factura/models/account_move.py
@@ -123,6 +123,7 @@ class AccountMove(models.Model):
             if self.currency_id != self.company_id.currency_id:
                 amount+=spot['spot_amount']
             else:
+                log.info('spot: %s', spot)
                 amount+=spot['Amount']
         if self.partner_id.l10n_pe_edi_retention_type:
             amount+=self.l10n_pe_edi_retention_amount()
