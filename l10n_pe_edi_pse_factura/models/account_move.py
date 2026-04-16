d7256e90047dd6ac2d7d325310023517ef863cd3
Edwar Ruben <edwardmf94@gmail.com>
2026-04-16 00:52:27 -0500

    [FIX] se ajusta montos en calculo detracciones

--- l10n_pe_edi_pse_factura/models/account_move.py
+++ l10n_pe_edi_pse_factura/models/account_move.py
@@ -109,8 +109,8 @@ class AccountMove(models.Model):
             'PaymentMeansID': line.product_id.l10n_pe_withhold_code,
             'PayeeFinancialAccount': national_bank_account_number,
             'PaymentMeansCode': '999',
-            'spot_amount': float_round(self.amount_total * (max_percent/100.0), precision_rounding=2),
-            'Amount': float_repr(float_round(self.amount_total_signed * (max_percent/100.0), precision_rounding=2), precision_digits=2),
+            'spot_amount': float_round(self.amount_total * (max_percent/100.0), precision_rounding=0.01),
+            'Amount': float_repr(float_round(self.amount_total_signed * (max_percent/100.0), precision_rounding=0.1), precision_digits=2),
             'PaymentPercent': max_percent,
             'spot_message': "Operación sujeta al sistema de Pago de Obligaciones Tributarias-SPOT, Banco de la Nacion %s%% Cod Serv. %s" % (
                 line.product_id.l10n_pe_withhold_percentage, line.product_id.l10n_pe_withhold_code)
